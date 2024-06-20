5a7071acb3fac3b6d77a59b8796e53281c2bedfc
mpuvvada1 <166017179+mpuvvada1@users.noreply.github.com>
2024-06-20 10:42:13 +0530

    code.py

---
+++ code.py
@@ -0,0 +1,69 @@
+inputfile = "rana.txt"
+
+f = open(inputfile, "r")
+
+seq = f.read()
+
+seq = seq.replace("\n", "")
+
+seq = seq.replace("\r", "")
+
+def translate(seq):
+
+table = {
+
+'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+
+'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+
+'AAC':'N, 'AAT':'N', 'AAA':'K', 'AAG':'K',
+
+'AGC'S', 'AGT' 'S', 'AGA' 'R', 'AGG':'R',
+
+'CTAL', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+
+'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+
+'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+
+'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+
+'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V', 'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A', 'GAC':'D', 'GAT':'D', 'GAA':'E', 'GAG':'E', 'GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G', 'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S', 'TTC':'F', 'TTT':'F', 'TTA':'L', 'TTG':'L', 'TAC':'Y', 'TAT':'Y', 'TAA':', 'TAG':", 'TGC':'C', 'TGT':'C', 'TGA':'_', 'TGG': 'W',
+
+}
+
+protein =" "
+
+if len(seq)%3 == 0: 
+for i in range(0, len(seq), 3): 
+codon = seq[i:i + 3] protein+= table[codon]
+
+return protein
+
+def read_seq(inputfile):
+
+with open(inputfile, "r") as f:
+
+seq = f.read()
+
+seq = seq.replace("\n", "")
+
+seq = seq.replace("\r", "")
+
+retin seq= read_seq("amino_acid_sequence_original.txt") 
+
+prt= dna = read_seq("rana.txt")
+
+p = translate(dna[20:935])
+
+p == prt
+
+print(prt)
+
+print("\n")
+
+print(dna)
+
+print("\n")
+
+print(p)
